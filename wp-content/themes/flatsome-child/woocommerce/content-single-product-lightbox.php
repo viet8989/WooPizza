596397cbdc3f3cb85942175bec18a78930a9e062
Viet <hungviet150@gmail.com>
2025-10-15 18:12:36 +0700

    Select Whole Pizza 26

--- wp-content/themes/flatsome-child/woocommerce/content-single-product-lightbox.php
+++ wp-content/themes/flatsome-child/woocommerce/content-single-product-lightbox.php
@@ -940,10 +940,10 @@ do_action( 'wc_quick_view_after_single_product' );
 				if (viewType === 'paired') {
 					$('#btn-paired').trigger('click');
 					// Active tab right-toppings
-					@$('.topping-tab').removeClass('active');
-					@$('.topping-tab[data-tab="right-toppings"]').addClass('active');
-					@$('.tab-content').removeClass('active');
-					@$('#right-toppings').addClass('active');					
+					$('.topping-tab').removeClass('active');
+					$('.topping-tab[data-tab="right-toppings"]').addClass('active');
+					$('.tab-content').removeClass('active');
+					$('#right-toppings').addClass('active');					
 				} else {
 					$('#btn-whole').trigger('click');
 				}
