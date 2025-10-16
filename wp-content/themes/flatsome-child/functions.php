55767921fcef2b4dcef88a4fdf8a11e76a7ca5b8
Viet <hungviet150@gmail.com>
2025-10-17 05:05:28 +0700

    Update menu admin Pizzas 24

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1068,13 +1068,11 @@ function hide_categories_css_js() {
 
 				// Track when crosssell field is opened
 				$(document).on('select2:opening', '#crosssell_ids', function(e) {
-					console.log('Crosssell field opened');
 					isCrosssellActive = true;
 				});
 
 				// Track when crosssell field is closed
 				$(document).on('select2:closing', '#crosssell_ids', function(e) {
-					console.log('Crosssell field closed');
 					setTimeout(function() {
 						isCrosssellActive = false;
 					}, 500);
@@ -1086,14 +1084,9 @@ function hide_categories_css_js() {
 					if (settings.url && settings.url.indexOf('admin-ajax.php') > -1 &&
 					    settings.url.indexOf('woocommerce_json_search_products') > -1) {
 
-						console.log('Product search AJAX detected');
-						console.log('isCrosssellActive:', isCrosssellActive);
-						console.log('Original URL:', settings.url);
-
 						// Add field=crosssell to URL if crosssell is active
 						if (isCrosssellActive) {
 							settings.url += '&field=crosssell';
-							console.log('Added field=crosssell to URL:', settings.url);
 						}
 					}
 				});
@@ -1206,20 +1199,12 @@ function filter_products_by_field_type( $products ) {
 		),
 	) );
 
-	// Detect which field is being searched by checking HTTP_REFERER or a custom parameter
+	// Detect which field is being searched by checking custom parameter
 	$is_crosssell_search = false;
-
-	// Debug: Log the request parameters
-	error_log( 'Product Search - Field param: ' . ( isset( $_REQUEST['field'] ) ? $_REQUEST['field'] : 'not set' ) );
-	error_log( 'Product Search - All GET: ' . print_r( $_GET, true ) );
-	error_log( 'Product Search - All POST: ' . print_r( $_POST, true ) );
 
 	// Check if there's a custom parameter indicating this is cross-sell search
 	if ( isset( $_REQUEST['field'] ) && $_REQUEST['field'] === 'crosssell' ) {
 		$is_crosssell_search = true;
-		error_log( 'Cross-sell search detected - showing ONLY toppings' );
-	} else {
-		error_log( 'Regular search - EXCLUDING toppings' );
 	}
 
 	if ( $is_crosssell_search ) {
