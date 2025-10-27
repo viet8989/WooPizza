d4ddc14c3b4acad1e990f5c14df786dfe1aaaa13
Viet <hungviet150@gmail.com>
2025-10-27 23:26:51 +0700

    Menu always shows on top 12

--- wp-content/themes/flatsome-child/header.php
+++ wp-content/themes/flatsome-child/header.php
@@ -34,4 +34,4 @@
 
     <?php do_action( 'flatsome_after_header' ); ?>
 
-    <main id="main" class="<?php flatsome_main_classes(); ?>" style="padding-top: var(--header-height);">
+    <main id="main" class="<?php flatsome_main_classes(); ?>" style="padding-top: 80px;">
