7490173488e47671499b1caca91598c4825e6021
Viet <hungviet150@gmail.com>
2025-10-29 18:02:03 +0700

    Edit display cart 25

--- wp-content/themes/flatsome-child/woocommerce/cart/mini-cart.php
+++ wp-content/themes/flatsome-child/woocommerce/cart/mini-cart.php
@@ -77,7 +77,7 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 							$is_paired = true;
 							$icon_url = get_site_url() . '/wp-content/uploads/2025/10/pizza_half_active.png';
 							$paired_icon = sprintf(
-								'<img src="%s" alt="Paired Pizza" class="paired-pizza-icon" width="20" height="20"/>',
+								'<img src="%s" alt="Paired Pizza" class="paired-pizza-icon" style="width: 20px !important; height: 20px !important;" />',
 								esc_url( $icon_url )
 							);
 							$display_title = sprintf(
