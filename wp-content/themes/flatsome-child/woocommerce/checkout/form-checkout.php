482f51d0839f456afeefd126ec374a853c7f1271
Viet <hungviet150@gmail.com>
2025-11-06 14:12:33 +0700

    Form Checkout 4

--- wp-content/themes/flatsome-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/flatsome-child/woocommerce/checkout/form-checkout.php
@@ -34,16 +34,6 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	<h3>Chọn phương thức nhận hàng</h3>
 	<div class="delivery-options" style="display: flex; gap: 20px; margin-top: 15px;">
 		<label class="delivery-option" style="flex: 1; cursor: pointer;">
-			<input type="radio" name="delivery_method" value="pickup" id="delivery_pickup">
-			<span class="delivery-icon">
-				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-					<path d="M9 22V12H15V22" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-				</svg>
-			</span>
-			<span class="delivery-text" style="font-weight: 600;">PICKUP - Đến lấy tại cửa hàng</span>
-		</label>
-		<label class="delivery-option" style="flex: 1; cursor: pointer;">
 			<input type="radio" name="delivery_method" value="delivery" id="delivery_delivery" checked>
 			<span class="delivery-icon">
 				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -54,6 +44,16 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 				</svg>
 			</span>
 			<span class="delivery-text" style="font-weight: 600;">DELIVERY - Giao hàng tận nơi</span>
+		</label>
+		<label class="delivery-option" style="flex: 1; cursor: pointer;">
+			<input type="radio" name="delivery_method" value="pickup" id="delivery_pickup">
+			<span class="delivery-icon">
+				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					<path d="M9 22V12H15V22" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+			</span>
+			<span class="delivery-text" style="font-weight: 600;">PICKUP - Đến lấy tại cửa hàng</span>
 		</label>
 	</div>
 </div>
@@ -225,8 +225,8 @@ jQuery(document).ready(function($) {
 		// Update hidden field
 		$('#selected_delivery_method').val(method);
 
-		// // Update shipping method
-		// $(document.body).trigger('update_checkout');
+		// Update shipping method
+		$(document.body).trigger('update_checkout');
 	});
 
 	// Set initial state
