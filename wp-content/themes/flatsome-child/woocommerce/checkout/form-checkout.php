6d8bbe19ad29f4c6cf9a823183ec1bf247abf963
Viet <hungviet150@gmail.com>
2025-11-06 15:45:33 +0700

    Form Checkout 9

--- wp-content/themes/flatsome-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/flatsome-child/woocommerce/checkout/form-checkout.php
@@ -85,6 +85,26 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		</div>
 
 		<div class="large-5 col">
+			<?php if ( function_exists( 'flatsome_sticky_column_open' ) ) { flatsome_sticky_column_open( 'checkout_sticky_sidebar' ); } ?>
+
+				<div class="col-inner">
+					<div class="checkout-sidebar sm-touch-scroll">
+
+						<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+
+						<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+
+						<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+
+						<div id="order_review" class="woocommerce-checkout-review-order">
+							<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+						</div>
+
+						<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+					</div>
+				</div>
+
+			<?php if ( function_exists( 'flatsome_sticky_column_close' ) ) { flatsome_sticky_column_close( 'checkout_sticky_sidebar' ); } ?>
 		</div>
 	</div>
 
