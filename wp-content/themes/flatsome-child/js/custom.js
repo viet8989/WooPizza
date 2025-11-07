4af5c5da163272983cf2ecb13855ad4be16732d5
Viet <hungviet150@gmail.com>
2025-11-07 21:19:03 +0700

    Menu Header - Element right 5

--- wp-content/themes/flatsome-child/js/custom.js
+++ wp-content/themes/flatsome-child/js/custom.js
@@ -14,29 +14,29 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    const menuButton = document.querySelector('.menu-button');
-    const menuClose = document.querySelector('.menu-close');
-    const menuOpen = document.querySelector('.menu-open');
-    const header = document.querySelector('.header');
-    const body = document.body;
+    // const menuButton = document.querySelector('.menu-button');
+    // const menuClose = document.querySelector('.menu-close');
+    // const menuOpen = document.querySelector('.menu-open');
+    // const header = document.querySelector('.header');
+    // const body = document.body;
 
-    if (menuButton && menuOpen) {
-        menuButton.addEventListener('click', function() {
-            menuOpen.classList.add('show');
-            header.classList.add('show-menu');
-            body.classList.add('hidden-show');
-            menuOpen.classList.remove('hide'); // Xóa class hide nếu có
-        });
-    }
+    // if (menuButton && menuOpen) {
+    //     menuButton.addEventListener('click', function() {
+    //         menuOpen.classList.add('show');
+    //         header.classList.add('show-menu');
+    //         body.classList.add('hidden-show');
+    //         menuOpen.classList.remove('hide'); // Xóa class hide nếu có
+    //     });
+    // }
 
-    if (menuClose && menuOpen) {
-        menuClose.addEventListener('click', function() {
-            menuOpen.classList.add('hide');
-            body.classList.remove('hidden-show');
-            menuOpen.classList.remove('show'); // Xóa class show nếu có
-            header.classList.remove('show-menu'); // Xóa class show nếu có
-        });
-    }
+    // if (menuClose && menuOpen) {
+    //     menuClose.addEventListener('click', function() {
+    //         menuOpen.classList.add('hide');
+    //         body.classList.remove('hidden-show');
+    //         menuOpen.classList.remove('show'); // Xóa class show nếu có
+    //         header.classList.remove('show-menu'); // Xóa class show nếu có
+    //     });
+    // }
 
     // Center flickity slider container on delivery page
     const rowSlider = document.querySelector('.row-slider');
